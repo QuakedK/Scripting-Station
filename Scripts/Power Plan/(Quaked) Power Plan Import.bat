19c06191d79e7f34f329ba6bc5abae5878ee1146
Quaked <124531365+QuakedK@users.noreply.github.com>
2026-03-02 16:15:33 -0600

    Update download URL for Power Plans zip file

--- Scripts/Power Plan/(Quaked) Power Plan Import.bat
+++ Scripts/Power Plan/(Quaked) Power Plan Import.bat
@@ -17,7 +17,7 @@ if not %errorlevel% == 0 (
 )
 
 :: (Quaked) Downloading Power Plan at Start.
-set "fileURL=https://github.com/QuakedK/Oneclick/raw/refs/heads/main/Downloads/PowerPlans.zip"
+set "fileURL=https://github.com/QuakedK/Oneclick/raw/refs/heads/main/Downloads/V8.0/PowerPlans.zip"
 set "fileName=Power Plan.zip"
 set "extractFolder=C:\Power Plan"
 set "downloadsFolder=C:\"
